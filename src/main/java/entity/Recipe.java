afb0e07cb79ef855cc506513cd855ca6863919a9
EddyZ4494 <eddyzhang6601@gmail.com>
2025-11-16 17:55:22 -0500

    Recipe entity update all getter/setter

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -17,6 +17,8 @@ public class Recipe {
     private int saves;
     private List<Review> reviews;
 
+    // --- Getters and Setters ---
+
     public String getRecipeName() {
         return recipeName;
     }
@@ -24,4 +26,73 @@ public class Recipe {
         this.recipeName = recipeName;
     }
 
+    public int getRecipeId() {
+        return recipeId;
+    }
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isShareable() {
+        return shareable;
+    }
+    public void setShareable(boolean shareable) {
+        this.shareable = shareable;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public int getViews() {
+        return views;
+    }
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getSaves() {
+        return saves;
+    }
+    public void setSaves(int saves) {
+        this.saves = saves;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
