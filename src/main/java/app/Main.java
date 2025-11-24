138bd52ec37fe8a08485eca8512640d83ac51e85
Oliver Kwun-Morfitt <56986548+olliekm@users.noreply.github.com>
2025-11-24 13:27:28 -0500

    Revert "added post recipe: view, view model, state and presenter"

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,11 +9,9 @@ public class Main {
                 .addLoginView()
                 .addSignupView()
                 .addRecipeSearchView()
-                .addPostRecipeView()
                 .addLoginUseCase()
                 .addSignupUseCase()
                 .build();
-
 
         application.pack();
         application.setLocationRelativeTo(null);
